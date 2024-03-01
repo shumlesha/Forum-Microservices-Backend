ddbb3c2194a48343417f6fe9d43b93b5db89a5e3
Alexey Shumkov <shumleshas@gmail.com>
2024-03-01 13:17:06 +0700

    Подключил валидацию

--- forum-app/src/main/java/com/example/forum/controller/CategoryController.java
+++ forum-app/src/main/java/com/example/forum/controller/CategoryController.java
@@ -1,9 +1,14 @@
 package com.example.forum.controller;
 
 
+import com.example.forum.dto.Category.CreateCategoryModel;
 import com.example.forum.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<?> createCategory(@Validated @RequestBody CreateCategoryModel createCategoryModel) {
+        categoryService.createCategory(createCategoryModel);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- forum-app/src/main/java/com/example/forum/service/CategoryService.java
+++ forum-app/src/main/java/com/example/forum/service/CategoryService.java
@@ -1,4 +1,7 @@
 package com.example.forum.service;
 
+import com.example.forum.dto.Category.CreateCategoryModel;
+
 public interface CategoryService {
+    void createCategory(CreateCategoryModel createCategoryModel);
 }
