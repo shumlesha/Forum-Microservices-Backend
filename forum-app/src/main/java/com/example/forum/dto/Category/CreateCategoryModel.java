d51ce689f82422ed5c35150d8647d4cb104defe2
Alexey Shumkov <shumleshas@gmail.com>
2024-03-01 13:17:06 +0700

    Подключил валидацию

---
+++ forum-app/src/main/java/com/example/forum/dto/Category/CreateCategoryModel.java
@@ -0,0 +1,21 @@
+package com.example.forum.dto.Category;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateCategoryModel {
+
+    @NotBlank(message = "Название категории не может быть пустым")
+    @Length(min = 1, message = "Название категории должно состоять хотя-бы из 1 символа")
+    private String name;
+
+    private UUID parentId;
+}
--- forum-app/src/main/java/com/example/forum/service/impl/CategoryServiceImpl.java
+++ forum-app/src/main/java/com/example/forum/service/impl/CategoryServiceImpl.java
@@ -1,8 +1,13 @@
 package com.example.forum.service.impl;
 
+import com.example.forum.dto.Category.CreateCategoryModel;
 import com.example.forum.service.CategoryService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+    @Override
+    public void createCategory(CreateCategoryModel createCategoryModel) {
+
+    }
 }
